bbfe144139a982e5a0513b1d9006818990aba64a
roseiliend <roseiliend@gmail.com>
2023-12-28 19:43:17 +0800

    fix wasm build

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,8 @@
 use bridge::{VmInput, execute_vm, Artifacts};
 use wasm_bindgen::prelude::*;
 
-// mod deserializer;
-// use deserializer::from_slice;
+mod deserializer;
+use deserializer::from_slice;
 
 extern "C" {
     pub fn wasm_input(is_public: u32) -> u64;
@@ -39,55 +39,55 @@ pub fn wasm_dbg_str(s: &str) {
     }
 }
 
-/* 
+
 fn get_input() -> VmInput {
-    // let length = unsafe { wasm_input(1) };
-    // let data = wasm_read_u8(length, 0);
-    // let words: &[u32] = bytemuck::cast_slice(data.as_slice());
-    // from_slice(words).unwrap()
-    VmInput {
-        header: Default::default(),
-        state_trie: Default::default(),
-        storage_trie: Default::default(),
-        contracts: Default::default(),
-        block_hashes: Default::default(),
-        poc_contract: Default::default(),
-        artifacts: Artifacts {
-            storage: Default::default(),
-            initial_balance: Default::default(),
-        }
-    }
+    let length = unsafe { wasm_input(1) };
+    let data = wasm_read_u8(length, 0);
+    let words: &[u32] = bytemuck::cast_slice(data.as_slice());
+    from_slice(words).unwrap()
+    // VmInput {
+    //     header: Default::default(),
+    //     state_trie: Default::default(),
+    //     storage_trie: Default::default(),
+    //     contracts: Default::default(),
+    //     block_hashes: Default::default(),
+    //     poc_contract: Default::default(),
+    //     artifacts: Artifacts {
+    //         storage: Default::default(),
+    //         initial_balance: Default::default(),
+    //     }
+    // }
 }
-*/
 
-// fn wasm_read_u8(length: u64, is_public: u32) -> Vec<u8> {
-//     let mut i = 0;
 
-//     let mut bytes: Vec<u8> = Vec::with_capacity(length.try_into().unwrap());
+fn wasm_read_u8(length: u64, is_public: u32) -> Vec<u8> {
+    let mut i = 0;
 
-//     while i * 8 < length {
-//         if i * 8 + 8 < length {
-//             unsafe {
-//                 let v = wasm_input(is_public);
-//                 v.to_le_bytes().iter().for_each(|x| bytes.push(*x))
-//             }
-//         } else {
-//             unsafe {
-//                 let v = wasm_input(is_public);
-//                 let left = (length - i * 8).try_into().unwrap();
-//                 v.to_le_bytes()[0..left].iter().for_each(|x| bytes.push(*x))
-//             }
-//         }
+    let mut bytes: Vec<u8> = Vec::with_capacity(length.try_into().unwrap());
 
-//         i += 1;              
-//     }
-//     return bytes;
-// }
+    while i * 8 < length {
+        if i * 8 + 8 < length {
+            unsafe {
+                let v = wasm_input(is_public);
+                v.to_le_bytes().iter().for_each(|x| bytes.push(*x))
+            }
+        } else {
+            unsafe {
+                let v = wasm_input(is_public);
+                let left = (length - i * 8).try_into().unwrap();
+                v.to_le_bytes()[0..left].iter().for_each(|x| bytes.push(*x))
+            }
+        }
+
+        i += 1;              
+    }
+    return bytes;
+}
 
 
 #[wasm_bindgen]
-pub fn zkmain() -> u64 {
-    // let vm_input = get_input();
-    // execute_vm(vm_input);
-    return 0;
+pub fn zkmain(){
+    let vm_input = get_input();
+    execute_vm(vm_input);
+
 }
